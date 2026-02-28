d5ef490c6ff359d708db908dd6749897a18cfb1e
MrMalachi <brownmnb11@yahoo.com>
2026-02-27 20:58:53 -0500

    Updated directory routing

---
+++ services/budget_tracker.py
@@ -0,0 +1,9 @@
+from projects.budget_mate.core.budget_allocator import BudgetAllocator
+
+class BudgetTracker(BudgetAllocator):
+    """
+    A child class used to track the user's cash flow based on:
+    Debt, Needs, Wants.
+    """
+
+
