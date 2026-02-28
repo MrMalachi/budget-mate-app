6657ebd0de1bd5d8b492d7d8cd91e6c2a20f52cf
MrMalachi <brownmnb11@yahoo.com>
2026-02-27 20:57:48 -0500

    Changed path for import since moving and renaming both the folder location and file name.

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ Because my monthly income fluctuates, Budget Mate is designed to calculate my
 personal income based on a variation of the 50/30/20 rule (Debt/Needs/Wants).
 """
 # Imports the BudgetCalculator class from the helper.py file.
-from helper import BudgetCalculator
+from projects.budget_mate.core.budget_allocator import BudgetAllocator
 
 def main():
-    budget = BudgetCalculator()
+    budget = BudgetAllocator()
     budget.run_budget_mate()
 
 if __name__ == "__main__":
