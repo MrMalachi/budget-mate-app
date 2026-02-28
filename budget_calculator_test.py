2a95a17dba8ff4716b56c45bddd5b6bf4ef90892
MrMalachi <brownmnb11@yahoo.com>
2026-02-27 19:10:17 -0500

    Deleted 'budget_calculator_test.py' because I do not know how to test my entire clss yet. Also moved 'budget_mate_calculator.py to helper.py so naming conventions are being followed.

--- budget_calculator_test.py
+++
@@ -1,8 +0,0 @@
-from budget_calculator import BudgetCalculator
-
-def test_get_monthly_income():
-    """Test that a single response is stored properly."""
-    prompt = "Enter your monthly income:"
-
-
-
